3d96f54af535c12fe419b9d350eb35809e3575a3
Sämir Hamadi <samir.hamadi@elisa.fi>
2012-04-07 12:30:15 +0300

    Barricade and it's brick first draft.

---
+++ src/fi/samssi/creatures/Barricade.java
@@ -0,0 +1,14 @@
+package fi.samssi.creatures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Barricade {
+    private final List<Brick> bricks = new ArrayList<Brick>();
+
+    public List<Brick> getBricks() {
+        return bricks;
+    }
+
+    public void constructBarricade() {}
+}
---
+++ src/fi/samssi/creatures/Brick.java
@@ -0,0 +1,10 @@
+package fi.samssi.creatures;
+
+import org.anddev.andengine.entity.sprite.Sprite;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+
+public class Brick extends Sprite {
+    public Brick(final float pX, final float pY, final TextureRegion pTextureRegion) {
+        super(pX, pY, pTextureRegion);
+    }
+}
