1d5281af87aa7b41305f729576c95f3c909d0cd0
Sämir Hamadi <samir.hamadi@elisa.fi>
2012-04-07 21:38:32 +0300

    Refactor invaders scene model to fit in the spaceship and add it to the mix. Spaceship has sucky graphics :P

---
+++ src/fi/samssi/creatures/SpaceShip.java
@@ -0,0 +1,11 @@
+package fi.samssi.creatures;
+
+import org.anddev.andengine.entity.sprite.Sprite;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+
+public class SpaceShip extends Sprite {
+
+    public SpaceShip(final float pX, final float pY, final TextureRegion pTextureRegion) {
+        super(pX, pY, pTextureRegion);
+    }
+}
