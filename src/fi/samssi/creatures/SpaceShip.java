68616aa6cff779e1c9fca3515c059ee13f53e7ad
Sämir Hamadi <samir.hamadi@elisa.fi>
2012-04-09 13:58:51 +0300

    Remove whitespace.

--- src/fi/samssi/creatures/SpaceShip.java
+++ src/fi/samssi/creatures/SpaceShip.java
@@ -4,7 +4,6 @@ import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.opengl.texture.region.TextureRegion;
 
 public class SpaceShip extends Sprite {
-
     public SpaceShip(final float pX, final float pY, final TextureRegion pTextureRegion) {
         super(pX, pY, pTextureRegion);
     }
